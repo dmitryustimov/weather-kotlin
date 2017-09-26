57ed6f165039f7ebe6899a4c61f7173bb81ce4b3
Dmitry Ustimov <dustimov@globus-ltd.com>
2017-09-26 17:50:59 +0300

    Bottom navigation bar

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-#Simple weather app
+# Simple weather app
 
 Put your openweathermap.org api key to <tt>gradle.properties</tt>
 ```groovy
--- app/src/main/java/ru/ustimov/weather/ui/MainActivity.kt
+++ app/src/main/java/ru/ustimov/weather/ui/MainActivity.kt
@@ -1,9 +1,10 @@
 package ru.ustimov.weather.ui
 
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import ru.ustimov.weather.R
-import ru.ustimov.weather.ui.pages.PagesFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,12 +12,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_bar);
+        bottomNavigationView.setOnNavigationItemSelectedListener({
+            Log.d("1111", "Selected ${it.title}")
+            true
+        })
+        bottomNavigationView.setOnNavigationItemReselectedListener({
+            Log.d("1111", "Reselected ${it.title}")
+        })
+
         if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
+            bottomNavigationView.selectedItemId = R.id.action_my_location
+            /*supportFragmentManager.beginTransaction()
                     .replace(R.id.content, PagesFragment.create())
-                    .commit()
+                    .commit()*/
         }
     }
-
 
 }
